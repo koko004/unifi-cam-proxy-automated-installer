e423f0b66e62bed6f165bf64d40e9bf815167237
koko004 <koko004@gmail.com>
2022-08-28 21:20:28 +0200

    Update automated-installer-unfi-cam-proxy.sh

--- automated-installer-unfi-cam-proxy.sh
+++ automated-installer-unfi-cam-proxy.sh
@@ -105,7 +105,6 @@ openssl req -new -sha256 -key /tmp/private.key -out /tmp/server.csr -subj "/C=TW
 openssl x509 -req -sha256 -days 36500 -in /tmp/server.csr -signkey /tmp/private.key -out /tmp/public.key
 cat /tmp/private.key /tmp/public.key > client.pem
 rm -f /tmp/private.key /tmp/public.key /tmp/server.csr
-cd ..
 echo 'ALL RECREATED'
 pwd
 cd unifi-cam-proxy1 && docker-compose up -d
